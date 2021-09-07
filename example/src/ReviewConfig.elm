b7103ea0cb81d2c76b4a7ee570c0c12f5a49bc6d
lue <lue.the.bird@gmail.com>
2021-09-07 11:47:40 +0200

    ↻ example

--- example/src/ReviewConfig.elm
+++ example/src/ReviewConfig.elm
@@ -11,11 +11,11 @@ when inside the directory containing this file.
 
 -}
 
-import NoTypeAliasConstructorCall
+import NoRecordAliasConstructor
 import Review.Rule exposing (Rule)
 
 
 config : List Rule
 config =
-    [ NoTypeAliasConstructorCall.rule
+    [ NoRecordAliasConstructor.rule
     ]
